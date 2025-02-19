e8f3a6f3c2032614dfb84353c209156d6af35625
Shafagh Sepehr <shafaghsepehr2003@gmail.com>
2025-02-19 09:50:05 +0330

    feat(Common): create LogicalResources file and add needed logical resources

---
+++ Common/LogicalResources.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using SystemGroup.Framework.Security;
+
+namespace SystemGroup.General.UniversityManagement.Common
+{
+    internal class LogicalResources : ILogicalResourceDeclarator
+    {
+        public void DeclareLogicalResourceTree(IList<LogicalResourceTreeNode> list)
+        {
+            var res = new LogicalResourceCategory("MyCategory", " Labels_University", new LogicalResourceTreeNode[]
+            {
+                new CompositeLogicalResource("EducationalAdministrator", "Labels_EducationalAdministrator"),
+                new CompositeLogicalResource("Student", "Labels_Student"),
+                new CompositeLogicalResource("Instructor", "Labels_Instructor"),
+            });
+            list.Add(res);
+        }
+    }
+}
