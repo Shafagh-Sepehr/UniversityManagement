4782abe57aab3e551c81ee76498889e6ab674c2c
Shafagh Sepehr <shafaghsepehr2003@gmail.com>
2025-02-20 18:42:21 +0330

    feat(Common): override delete load options to allow cascading deletion of time slot for presentation

--- Common/Presentaion/Presentation.cs
+++ Common/Presentaion/Presentation.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using SystemGroup.Framework.Business;
 using SystemGroup.Framework.Common;
 using SystemGroup.Framework.MetaData;
 using SystemGroup.Framework.MetaData.Mapping;
@@ -11,21 +12,22 @@ namespace SystemGroup.General.UniversityManagement.Common
     [SearchFields(nameof(Capacity))]
     partial class Presentation : Entity
     {
-        //#region Properties
-
-        //public string CourseTitle { get; set; }
-
-        //#endregion
+        public override DetailLoadOptions DeleteLoadOptions
+        {
+            get
+            {
+                return LoadOptions.With<Presentation>(c => c.TimeSlots);
+            }
+        }
 
         #region Methods
 
-        
+
         public override void GetColumns(List<ColumnInfo> columns)
         {
             base.GetColumns(columns);
 
             columns.Add(new NumericColumnInfo(nameof(Capacity), "Presentation_Capacity", NumericType.Integer));
-            //columns.Add(new TextColumnInfo(nameof(CourseTitle), "Presentation_CourseTitle"));
             columns.Add(new ReferenceColumnInfo(nameof(CourseRef), "_"));
             columns.Add(new ReferenceColumnInfo(nameof(SemesterRef), "_"));
             columns.Add(new ReferenceColumnInfo(nameof(InstructorRef), "_"));
